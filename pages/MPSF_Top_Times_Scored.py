0eb2665f99cd777dd4447729c0ba85cc88c3eddc
Daniel Pry <62821512+danielpry@users.noreply.github.com>
2023-01-11 18:46:34 -0800

    Add files via upload

--- pages/MPSF_Top_Times_Scored.py
+++ pages/MPSF_Top_Times_Scored.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import altair as alt
 
 with st.container():
     st.title('MPSF Top Times Scored')
@@ -12,8 +13,9 @@ with st.container():
         'Event:',
         ('50 Fr', '100 Fr', '200 Fr', '500 Fr', '1650 Fr',
         '100 Bk', '200 Bk', '100 Br', '200 Br', '100 Fly',
-        '200 Fly', '200 IM', '400 IM', '1 Meter', '3 Meter',
-        'Platform')
+        '200 Fly', '200 IM', '400 IM', '200 Medley Relay',
+        '400 Medley Relay', '200 Free Relay', '400 Free Relay',
+        '800 Free Relay', '1 Meter', '3 Meter', 'Platform')
     )
 
 top_times = pd.read_csv('MPSF_' + gen.lower() + '.csv', index_col=False).drop(columns='Unnamed: 0')
@@ -48,6 +50,16 @@ with col1:
         st.table(top_times[top_times['Event'] == '200 Individual Medley SCY'])
     if event == '400 IM':
         st.table(top_times[top_times['Event'] == '400 Individual Medley SCY'])
+    if event == '200 Medley Relay':
+        st.table(top_times[top_times['Event'] == '200 Medley Relay'])
+    if event == '400 Medley Relay':
+        st.table(top_times[top_times['Event'] == '400 Medley Relay'])
+    if event == '200 Free Relay':
+        st.table(top_times[top_times['Event'] == '200 Free Relay'])
+    if event == '400 Free Relay':
+        st.table(top_times[top_times['Event'] == '400 Free Relay'])
+    if event == '800 Free Relay':
+        st.table(top_times[top_times['Event'] == '800 Free Relay'])
     if event == '1 Meter':
         st.table(top_times[top_times['Event'] == '1 Meter'])
     if event == '3 Meter':
@@ -83,6 +95,16 @@ with col2:
         st.table(pd.read_csv('200 Individual Medley SCY_' + gen.lower() + '_points.csv', index_col=False).sort_values(['Points'], ascending=[False]))
     if event == '400 IM':
         st.table(pd.read_csv('400 Individual Medley SCY_' + gen.lower() + '_points.csv', index_col=False).sort_values(['Points'], ascending=[False]))
+    if event == '200 Medley Relay':
+        st.table(pd.read_csv('200 Medley Relay_' + gen.lower() + '_points.csv', index_col=False).sort_values(['Points'], ascending=[False]))
+    if event == '400 Medley Relay':
+        st.table(pd.read_csv('400 Medley Relay_' + gen.lower() + '_points.csv', index_col=False).sort_values(['Points'], ascending=[False]))
+    if event == '200 Free Relay':
+        st.table(pd.read_csv('200 Free Relay_' + gen.lower() + '_points.csv', index_col=False).sort_values(['Points'], ascending=[False]))
+    if event == '400 Free Relay':
+        st.table(pd.read_csv('400 Free Relay_' + gen.lower() + '_points.csv', index_col=False).sort_values(['Points'], ascending=[False]))
+    if event == '800 Free Relay':
+        st.table(pd.read_csv('800 Free Relay_' + gen.lower() + '_points.csv', index_col=False).sort_values(['Points'], ascending=[False]))
     if event == '1 Meter':
         st.table(pd.read_csv('1 Meter_' + gen.lower() + '_points.csv', index_col=False).sort_values(['Points'], ascending=[False]))
     if event == '3 Meter':
@@ -90,7 +112,24 @@ with col2:
     if event == 'Platform':
         st.table(pd.read_csv('Platform_' + gen.lower() + '_points.csv', index_col=False).sort_values(['Points'], ascending=[False]))
     
+    
     st.write('Scoring break down for all events:')
     st.table(top_times.groupby('Team')['Points'].sum().sort_values(ascending=False))
 
+
+with st.container():
+    plot = alt.Chart(top_times).mark_bar().encode(
+        x='sum(Points)',
+        y='Team',
+        color=alt.Color('Event', scale=alt.Scale(scheme='tableau20')),
+        order=alt.Order(
+            # Sort the segments of the bars by this field
+            'Event',
+            sort='ascending')
+    ).properties(
+        width=1850,
+        height=500
+    ).interactive()
+
     
+    st.altair_chart(plot)
