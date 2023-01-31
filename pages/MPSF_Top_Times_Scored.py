45a85876174b77840677ba49d3fb271e930e844b
Daniel Pry <danielpry@umail.ucsb.edu>
2023-01-30 22:14:00 -0800

    adding more fixes

--- pages/MPSF_Top_Times_Scored.py
+++ pages/MPSF_Top_Times_Scored.py
@@ -169,13 +169,10 @@ with st.container():
                 getEvents(top_times, 'University of California, Davis')
             )
             query = BYUW+UCSBW+UIWW+UCSDW+UHW+CSUBW+CPW+UOPW+USDW+UCDW
-    
-        st.write('Click arrow below to expand all queried entries')
-        st.write(query)
-    
-        if st.checkbox('Check box to use line ups above'):
+        try:
             top_times = GetCustomLineup(top_times, query)
-
+        except:
+            top_times = pd.DataFrame()
 
     event = st.selectbox(
         'Event:',
@@ -185,147 +182,150 @@ with st.container():
         '400 Medley Relay', '200 Free Relay', '400 Free Relay',
         '800 Free Relay', '1 Meter', '3 Meter', 'Platform')
     )
+try:
+    col1, col2 = st.columns(2)
 
-col1, col2 = st.columns(2)
+    with col1:
+        st.write('Top times in event:')
+        if event == '50 Fr':
+            st.table(top_times[top_times['Event'] == '50 Freestyle SCY'])
+        if event == '100 Fr':
+            st.table(top_times[top_times['Event'] == '100 Freestyle SCY'])
+        if event == '200 Fr':
+            st.table(top_times[top_times['Event'] == '200 Freestyle SCY'])
+        if event == '500 Fr':
+            st.table(top_times[top_times['Event'] == '500 Freestyle SCY'])
+        if event == '1650 Fr':
+            st.table(top_times[top_times['Event'] == '1650 Freestyle SCY'])
+        if event == '100 Bk':
+            st.table(top_times[top_times['Event'] == '100 Backstroke SCY'])
+        if event == '200 Bk':
+            st.table(top_times[top_times['Event'] == '200 Backstroke SCY'])
+        if event == '100 Br':
+            st.table(top_times[top_times['Event'] == '100 Breaststroke SCY'])
+        if event == '200 Br':
+            st.table(top_times[top_times['Event'] == '200 Breaststroke SCY'])
+        if event == '100 Fly':
+            st.table(top_times[top_times['Event'] == '100 Butterfly SCY'])
+        if event == '200 Fly':
+            st.table(top_times[top_times['Event'] == '200 Butterfly SCY'])
+        if event == '200 IM':
+            st.table(top_times[top_times['Event'] == '200 Individual Medley SCY'])
+        if event == '400 IM':
+            st.table(top_times[top_times['Event'] == '400 Individual Medley SCY'])
+        if event == '200 Medley Relay':
+            st.table(top_times[top_times['Event'] == '200 Medley Relay'])
+        if event == '400 Medley Relay':
+            st.table(top_times[top_times['Event'] == '400 Medley Relay'])
+        if event == '200 Free Relay':
+            st.table(top_times[top_times['Event'] == '200 Free Relay'])
+        if event == '400 Free Relay':
+            st.table(top_times[top_times['Event'] == '400 Free Relay'])
+        if event == '800 Free Relay':
+            st.table(top_times[top_times['Event'] == '800 Free Relay'])
+        if event == '1 Meter':
+            st.table(top_times[top_times['Event'] == '1 Meter'])
+        if event == '3 Meter':
+            st.table(top_times[top_times['Event'] == '3 Meter'])
+        if event == 'Platform':
+            st.table(top_times[top_times['Event'] == 'Platform'])
 
-with col1:
-    st.write('Top times in event:')
-    if event == '50 Fr':
-        st.table(top_times[top_times['Event'] == '50 Freestyle SCY'])
-    if event == '100 Fr':
-         st.table(top_times[top_times['Event'] == '100 Freestyle SCY'])
-    if event == '200 Fr':
-         st.table(top_times[top_times['Event'] == '200 Freestyle SCY'])
-    if event == '500 Fr':
-         st.table(top_times[top_times['Event'] == '500 Freestyle SCY'])
-    if event == '1650 Fr':
-         st.table(top_times[top_times['Event'] == '1650 Freestyle SCY'])
-    if event == '100 Bk':
-         st.table(top_times[top_times['Event'] == '100 Backstroke SCY'])
-    if event == '200 Bk':
-        st.table(top_times[top_times['Event'] == '200 Backstroke SCY'])
-    if event == '100 Br':
-        st.table(top_times[top_times['Event'] == '100 Breaststroke SCY'])
-    if event == '200 Br':
-        st.table(top_times[top_times['Event'] == '200 Breaststroke SCY'])
-    if event == '100 Fly':
-        st.table(top_times[top_times['Event'] == '100 Butterfly SCY'])
-    if event == '200 Fly':
-        st.table(top_times[top_times['Event'] == '200 Butterfly SCY'])
-    if event == '200 IM':
-        st.table(top_times[top_times['Event'] == '200 Individual Medley SCY'])
-    if event == '400 IM':
-        st.table(top_times[top_times['Event'] == '400 Individual Medley SCY'])
-    if event == '200 Medley Relay':
-        st.table(top_times[top_times['Event'] == '200 Medley Relay'])
-    if event == '400 Medley Relay':
-        st.table(top_times[top_times['Event'] == '400 Medley Relay'])
-    if event == '200 Free Relay':
-        st.table(top_times[top_times['Event'] == '200 Free Relay'])
-    if event == '400 Free Relay':
-        st.table(top_times[top_times['Event'] == '400 Free Relay'])
-    if event == '800 Free Relay':
-        st.table(top_times[top_times['Event'] == '800 Free Relay'])
-    if event == '1 Meter':
-        st.table(top_times[top_times['Event'] == '1 Meter'])
-    if event == '3 Meter':
-        st.table(top_times[top_times['Event'] == '3 Meter'])
-    if event == 'Platform':
-        st.table(top_times[top_times['Event'] == 'Platform'])
+    with col2:
+        st.write('Scoring break down per team:')
+        if event == '50 Fr':
+            st.table(top_times[top_times['Event'] == '50 Freestyle SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '100 Fr':
+            st.table(top_times[top_times['Event'] == '100 Freestyle SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '200 Fr':
+            st.table(top_times[top_times['Event'] == '200 Freestyle SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '500 Fr':
+            st.table(top_times[top_times['Event'] == '500 Freestyle SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '1650 Fr':
+            st.table(top_times[top_times['Event'] == '1650 Freestyle SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '100 Bk':
+            st.table(top_times[top_times['Event'] == '100 Backstroke SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '200 Bk':
+            st.table(top_times[top_times['Event'] == '200 Backstroke SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '100 Br':
+            st.table(top_times[top_times['Event'] == '100 Breaststroke SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '200 Br':
+            st.table(top_times[top_times['Event'] == '200 Breaststroke SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '100 Fly':
+            st.table(top_times[top_times['Event'] == '100 Butterfly SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '200 Fly':
+            st.table(top_times[top_times['Event'] == '200 Butterfly SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '200 IM':
+            st.table(top_times[top_times['Event'] == '200 Individual Medley SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '400 IM':
+            st.table(top_times[top_times['Event'] == '400 Individual Medley SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '200 Medley Relay':
+            st.table(top_times[top_times['Event'] == '200 Medley Relay'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '400 Medley Relay':
+            st.table(top_times[top_times['Event'] == '400 Medley Relay'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '200 Free Relay':
+            st.table(top_times[top_times['Event'] == '200 Free Relay'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '400 Free Relay':
+            st.table(top_times[top_times['Event'] == '400 Free Relay'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '800 Free Relay':
+            st.table(top_times[top_times['Event'] == '800 Free Relay'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '1 Meter':
+            st.table(top_times[top_times['Event'] == '1 Meter'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == '3 Meter':
+            st.table(top_times[top_times['Event'] == '3 Meter'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        if event == 'Platform':
+            st.table(top_times[top_times['Event'] == 'Platform'].groupby('Team')['Points'].sum().sort_values(ascending=False))
+        
+        
+        st.write('Scoring break down for all events:')
+        st.table(top_times.groupby('Team')['Points'].sum().sort_values(ascending=False))
 
-with col2:
-    st.write('Scoring break down per team:')
-    if event == '50 Fr':
-        st.table(top_times[top_times['Event'] == '50 Freestyle SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '100 Fr':
-        st.table(top_times[top_times['Event'] == '100 Freestyle SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '200 Fr':
-        st.table(top_times[top_times['Event'] == '200 Freestyle SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '500 Fr':
-        st.table(top_times[top_times['Event'] == '500 Freestyle SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '1650 Fr':
-        st.table(top_times[top_times['Event'] == '1650 Freestyle SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '100 Bk':
-        st.table(top_times[top_times['Event'] == '100 Backstroke SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '200 Bk':
-        st.table(top_times[top_times['Event'] == '200 Backstroke SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '100 Br':
-        st.table(top_times[top_times['Event'] == '100 Breaststroke SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '200 Br':
-        st.table(top_times[top_times['Event'] == '200 Breaststroke SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '100 Fly':
-        st.table(top_times[top_times['Event'] == '100 Butterfly SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '200 Fly':
-        st.table(top_times[top_times['Event'] == '200 Butterfly SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '200 IM':
-        st.table(top_times[top_times['Event'] == '200 Individual Medley SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '400 IM':
-        st.table(top_times[top_times['Event'] == '400 Individual Medley SCY'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '200 Medley Relay':
-        st.table(top_times[top_times['Event'] == '200 Medley Relay'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '400 Medley Relay':
-        st.table(top_times[top_times['Event'] == '400 Medley Relay'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '200 Free Relay':
-        st.table(top_times[top_times['Event'] == '200 Free Relay'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '400 Free Relay':
-        st.table(top_times[top_times['Event'] == '400 Free Relay'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '800 Free Relay':
-        st.table(top_times[top_times['Event'] == '800 Free Relay'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '1 Meter':
-        st.table(top_times[top_times['Event'] == '1 Meter'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == '3 Meter':
-        st.table(top_times[top_times['Event'] == '3 Meter'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    if event == 'Platform':
-        st.table(top_times[top_times['Event'] == 'Platform'].groupby('Team')['Points'].sum().sort_values(ascending=False))
-    
-    
-    st.write('Scoring break down for all events:')
-    st.table(top_times.groupby('Team')['Points'].sum().sort_values(ascending=False))
+    # visual
+    with st.container():
+        plot = alt.Chart(top_times).mark_bar().encode(
+            x='sum(Points)',
+            y='Team',
+            color=alt.Color('Event', scale=alt.Scale(scheme='tableau20')),
+            order=alt.Order(
+                # Sort the segments of the bars by this field
+                'Event',
+                sort='ascending')
+        ).properties(
+            width=1850,
+            height=500
+        ).interactive()
 
-# visual
-with st.container():
-    plot = alt.Chart(top_times).mark_bar().encode(
-        x='sum(Points)',
-        y='Team',
-        color=alt.Color('Event', scale=alt.Scale(scheme='tableau20')),
-        order=alt.Order(
-            # Sort the segments of the bars by this field
-            'Event',
-            sort='ascending')
-    ).properties(
-        width=1850,
-        height=500
-    ).interactive()
+        st.write('Plot of points by event')
+        st.altair_chart(plot, use_container_width=True)
 
-    st.write('Plot of points by event')
-    st.altair_chart(plot, use_container_width=True)
+        # indivdual points
+        x = top_times['Name'].value_counts()
+        x = pd.DataFrame(data=x).reset_index().rename(columns={'index':'Name', 'Name':'Number of Events'})
+        num_events = pd.merge(top_times, x, on='Name', how='outer')
+        pts = num_events.groupby(['Name'])['Points'].sum()
+        pts = pd.DataFrame(data=pts).reset_index().rename(columns={'Points':'Total Points'})
+        final = pd.merge(num_events, pts, on='Name', how='outer').sort_values(by='Number of Events', ascending=False)
 
-    # indivdual points
-    x = top_times['Name'].value_counts()
-    x = pd.DataFrame(data=x).reset_index().rename(columns={'index':'Name', 'Name':'Number of Events'})
-    num_events = pd.merge(top_times, x, on='Name', how='outer')
-    pts = num_events.groupby(['Name'])['Points'].sum()
-    pts = pd.DataFrame(data=pts).reset_index().rename(columns={'Points':'Total Points'})
-    final = pd.merge(num_events, pts, on='Name', how='outer').sort_values(by='Number of Events', ascending=False)
+        if gen == 'Women':
+            team_list = ['University of Hawaii', 'UC San Diego', 'UC Santa Barbara',
+        'University of San Diego', 'Brigham Young University',
+        'California State University Bakersfield',
+        'University of California, Davis', 'University of Incarnate Word',
+        'Cal Poly', 'University of the Pacific']
+        if gen == 'Men':
+            team_list = ['University of Hawaii', 'UC San Diego', 'UC Santa Barbara', 
+            'Brigham Young University', 'California State University Bakersfield',
+        'University of Incarnate Word', 'Cal Poly', 'University of the Pacific']
 
-    if gen == 'Women':
-        team_list = ['University of Hawaii', 'UC San Diego', 'UC Santa Barbara',
-       'University of San Diego', 'Brigham Young University',
-       'California State University Bakersfield',
-       'University of California, Davis', 'University of Incarnate Word',
-       'Cal Poly', 'University of the Pacific']
-    if gen == 'Men':
-        team_list = ['University of Hawaii', 'UC San Diego', 'UC Santa Barbara', 
-        'Brigham Young University', 'California State University Bakersfield',
-       'University of Incarnate Word', 'Cal Poly', 'University of the Pacific']
+        team = st.selectbox(
+            'Team:',
+            top_times['Team'].unique()
+        )
+        
+        show_final = final[(final['Team'] == team) & (final['Gender'] == gen2)].drop(columns=['Gender', 'Team'])
+        show_final = show_final[show_final['Name'].str.contains(team) == False]
 
-    team = st.selectbox(
-        'Team:',
-        team_list
-    )
-    
-    show_final = final[(final['Team'] == team) & (final['Gender'] == gen2)].drop(columns=['Gender', 'Team'])
-    show_final = show_final[show_final['Name'].str.contains(team) == False]
+        st.write('Number of events per swimmer:')
+        st.table(show_final)
 
-    st.write('Number of events per swimmer:')
-    st.table(show_final)
+except:
+    pass
